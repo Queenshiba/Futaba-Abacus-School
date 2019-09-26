d252d353002c9bcd49b272860e1a80f0fac0def3
Queenshiba <haruka0111.2a@gmail.com>
2019-09-26 17:16:28 +0900

    Updates

---
+++ main.js
@@ -0,0 +1,32 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function mapAccess() {
+    var myCenter = new google.maps.LatLng(34.786510, 135.389497);
+    var mapCanvas = document.getElementById('maps');
+    var mapOptions = {
+    center: myCenter,
+    zoom: 16
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({
+    position: myCenter 
+    });
+    marker.setMap(map);
+    }
+
+  // hamburger nav 
+
